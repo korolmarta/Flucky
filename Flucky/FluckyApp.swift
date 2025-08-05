16de964eb4f0fe4f5a5177fe19c570fb1a6ec4fa
Marta Korol <korolmarta490@gmail.com>
2025-08-05 12:17:21 +0300

    Create project

---
+++ Flucky/FluckyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FluckyApp.swift
+//  Flucky
+//
+//  Created by Marta Korol on 05.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct FluckyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
